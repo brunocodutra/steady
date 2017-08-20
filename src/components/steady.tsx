8f8da3cc72e422fbc4c66677e89f0dadbfa0c22c
Bruno Dutra <brunocodutra@gmail.com>
2017-08-20 13:33:04 +0200

    add <Toolbar/>

--- src/components/steady.tsx
+++ src/components/steady.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
 import Schematics from 'components/schematics';
+import Toolbar from 'components/toolbar';
 
 export default class extends React.Component< {}, {} > {
   public render() {
     return (
       <div className='steady d-flex flex-column flex-grow'>
+        <Toolbar/>
         <Schematics/>
       </div>
     );
---
+++ src/components/toolbar.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class extends React.Component< {}, {} > {
+  public render() {
+    return (
+      <div className='toolbar collapse d-md-block'>
+        <div className='board d-flex flex-row mb-2'/>
+      </div>
+    );
+  }
+}
