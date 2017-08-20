b3df7c58c622b44208a2930a0e9f7a6fd0bf7f6b
Bruno Dutra <brunocodutra@gmail.com>
2017-08-20 16:59:00 +0200

    connect <Schematics/> to the Store

--- src/components/schematics.tsx
+++ src/components/schematics.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import {connect} from 'react-redux';
 
-export default class extends React.Component< {}, {} > {
-  public render() {
-    return (
-      <div className='schematics board flex-grow border rounded p-3'/>
-    );
-  }
-}
+import {Model} from 'model';
+import {State} from 'reducer';
+
+type Props = {
+  readonly entry: Model,
+};
+
+const mapState = ({schematics}: State): Props => ({
+  entry: schematics,
+});
+
+export default connect(mapState)(
+  class extends React.Component< Props, {} > {
+    public render() {
+      return (
+        <div className='schematics board flex-grow border rounded p-3'/>
+      );
+    }
+});
