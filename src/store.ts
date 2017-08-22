f364401ad927c7ec9f97a19291ac339dbbc391ec
Bruno Dutra <brunocodutra@gmail.com>
2017-08-22 22:54:48 +0200

    setup redux-logger

--- src/store.ts
+++ src/store.ts
@@ -2,4 +2,13 @@ import * as Redux from 'redux';
 
 import {reducer} from 'reducer';
 
-export const store = Redux.createStore(reducer);
+const middleware: Redux.Middleware[] = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(require('redux-logger').default);
+}
+
+export const store = Redux.createStore(
+  reducer,
+  Redux.applyMiddleware(...middleware),
+);
