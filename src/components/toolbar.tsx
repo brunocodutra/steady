03980cd592d47a1d4cc05711441460228a815a45
Bruno Dutra <brunocodutra@gmail.com>
2017-08-26 18:34:20 +0200

    add vsrc to the toolbar

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 
+import Tool from 'components/tool';
+import {Models} from 'model';
+
 export default class extends React.Component< {}, {} > {
   public render() {
     return (
       <div className='toolbar collapse d-md-block'>
-        <div className='board d-flex flex-row mb-2'/>
+        <div className='board d-flex flex-row pb-1 mb-1'>
+          <Tool kind={Models.vsrc}/>
+        </div>
       </div>
     );
   }
