06def93cab6d1ecf75d927c1a8abe4d7f96851a8
Bruno Dutra <brunocodutra@gmail.com>
2017-08-29 20:38:17 +0200

    improve layout responsiveness

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -7,14 +7,16 @@ export default class extends React.Component< {}, {} > {
   public render() {
     return (
       <div className='toolbar collapse d-md-block'>
-        <div className='board d-flex flex-row pb-1 mb-1'>
-          <Tool kind={Models.vsrc}/>
-          <Tool kind={Models.isrc}/>
-          <Tool kind={Models.impedance}/>
-          <Tool kind={Models.admittance}/>
-          <Tool kind={Models.xformer}/>
-          <Tool kind={Models.xline}/>
-          <Tool kind={Models.shunt}/>
+        <div className='board d-flex pb-1 mb-2'>
+          <div className='d-flex flex-row mx-auto'>
+            <Tool kind={Models.vsrc}/>
+            <Tool kind={Models.isrc}/>
+            <Tool kind={Models.impedance}/>
+            <Tool kind={Models.admittance}/>
+            <Tool kind={Models.xformer}/>
+            <Tool kind={Models.xline}/>
+            <Tool kind={Models.shunt}/>
+          </div>
         </div>
       </div>
     );
