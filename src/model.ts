07bf87b561ca51644454d4a9fd65f5449a68aac3
Bruno Dutra <brunocodutra@gmail.com>
2017-09-03 13:09:07 +0200

    store the quadripole representation of components

--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,6 @@
+import {cosh, div, mul, neg, rect, sinh} from 'phasor';
+import {eye, Quadripole, quadripole} from 'quadripole';
+
 export enum Models {
   vsrc,
   isrc,
@@ -24,66 +27,84 @@ type Element = {
     | Models.knee
     | Models.connector
   ,
+
+  readonly params: Quadripole,
 };
 
 type Series = {
   readonly kind: Models.series,
   readonly components: Model[],
+  readonly params: Quadripole,
 };
 
 type Shunt = {
   readonly kind: Models.shunt,
-  readonly components: Model[],
   readonly indentation: number,
+  readonly components: Model[],
+  readonly params: Quadripole,
 };
 
 export type Model = Element | Series | Shunt;
 
 export const ModelFactory: {[kind: number]: (...args: any[]) => Model} = {
-  [Models.vsrc]: (): Element => ({
+  [Models.vsrc]: (v = rect(0)): Element => ({
     kind: Models.vsrc,
+    params: quadripole(eye, [v, rect(0)]),
   }),
 
-  [Models.isrc]: (): Element => ({
+  [Models.isrc]: (i = rect(0)): Element => ({
     kind: Models.isrc,
+    params: quadripole(eye, [rect(0), i]),
   }),
 
-  [Models.impedance]: (): Element => ({
+  [Models.impedance]: (z = rect(0)): Element => ({
     kind: Models.impedance,
+    params: quadripole([[rect(1), neg(z)], [rect(0), rect(1)]]),
   }),
 
-  [Models.admittance]: (): Element => ({
+  [Models.admittance]: (y = rect(0)): Element => ({
     kind: Models.admittance,
+    params: quadripole([[rect(1), rect(0)], [neg(y), rect(1)]]),
   }),
 
-  [Models.xformer]: (): Element => ({
+  [Models.xformer]: (n = 1): Element => ({
     kind: Models.xformer,
+    params: quadripole([[rect(n), rect(0)], [rect(0), rect(1 / n)]]),
   }),
 
-  [Models.xline]: (): Element => ({
+  [Models.xline]: (z = rect(1), y = rect(0)): Element => ({
     kind: Models.xline,
+    params: quadripole([
+      [cosh(y), neg(mul(z, sinh(y)))],
+      [neg(div(sinh(y), z)), cosh(y)],
+    ]),
   }),
 
   [Models.ground]: (): Element => ({
     kind: Models.ground,
+    params: quadripole(),
   }),
 
   [Models.knee]: (): Element => ({
     kind: Models.knee,
+    params: quadripole(),
   }),
 
   [Models.connector]: (): Element => ({
     kind: Models.connector,
+    params: quadripole(),
   }),
 
   [Models.series]: (): Series => ({
     kind: Models.series,
     components: [ModelFactory[Models.ground](), ModelFactory[Models.connector]()],
+    params: quadripole(),
   }),
 
   [Models.shunt]: (): Shunt => ({
     kind: Models.shunt,
-    components: [ModelFactory[Models.knee](), ModelFactory[Models.connector]()],
     indentation: 0,
+    components: [ModelFactory[Models.knee](), ModelFactory[Models.connector]()],
+    params: quadripole(),
   }),
 };
