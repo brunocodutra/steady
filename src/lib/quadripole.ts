6bcdf87a9bdf3dd4ed8990e01b615320a5905d6f
Bruno Dutra <brunocodutra@gmail.com>
2017-12-24 16:35:21 +0100

    add a custom type guard for Quadripole

--- src/lib/quadripole.ts
+++ src/lib/quadripole.ts
@@ -1,31 +1,46 @@
-import {add, div, mul, Phasor, rect, sub} from 'lib/phasor';
+import {add, div, isPhasor, mul, Phasor, rect, sub} from 'lib/phasor';
 
-type ABCD = [
-  [Phasor, Phasor],
-  [Phasor, Phasor]
-];
+type P2 = [Phasor, Phasor];
 
-type VI = [Phasor, Phasor];
+const isP2 = (p: any): p is P2 => (
+  typeof p === 'object' &&
+  '0' in p && isPhasor(p[0]) &&
+  '1' in p && isPhasor(p[1])
+);
+
+type P22 = [P2, P2];
+
+const isP22 = (p: any): p is P22 => (
+  typeof p === 'object' &&
+  '0' in p && isP2(p[0]) &&
+  '1' in p && isP2(p[1])
+);
 
 export type Quadripole = {
-  abcd: ABCD,
-  vi: VI,
+  abcd: P22,
+  vi: P2,
 };
+
+export const isQuadripole = (q: any): q is Quadripole => (
+  typeof q === 'object' &&
+  'abcd' in q && isP22(q.abcd) &&
+  'vi' in q && isP2(q.vi)
+);
 
 const _0 = rect(0);
 const _1 = rect(1);
 
-export const eye: ABCD = [[_1, _0], [_0, _1]];
-export const quadripole = (abcd = eye, vi: VI = [_0, _0]): Quadripole => ({abcd, vi});
+export const eye: P22 = [[_1, _0], [_0, _1]];
+export const quadripole = (abcd = eye, vi: P2 = [_0, _0]): Quadripole => ({abcd, vi});
 
-export const project = ({abcd: [[a, b], [c, d]], vi: [u, j]}: Quadripole, [v, i]: VI): VI => {
+export const project = ({abcd: [[a, b], [c, d]], vi: [u, j]}: Quadripole, [v, i]: P2): P2 => {
   return [
     add(add(mul(a, v), mul(b, i)), u),
     add(add(mul(c, v), mul(d, i)), j),
   ];
 };
 
-export const solve = ({abcd: [[a, b], [c, d]], vi: [u, j]}: Quadripole, [vi, io]: VI = [_0, _0]): VI => {
+export const solve = ({abcd: [[a, b], [c, d]], vi: [u, j]}: Quadripole, [vi, io]: P2 = [_0, _0]): P2 => {
   const ii = div(sub(sub(io, j), mul(c, vi)), d);
   const vo = add(add(mul(a, vi), mul(b, ii)), u);
 
