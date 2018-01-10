1160f4225e21d0fa2752a52d4bc4bff4e1c993ed
Bruno Dutra <brunocodutra@gmail.com>
2018-01-10 23:02:29 +0100

    handle the Action to remove existing Elements

--- src/reducer/schematics.ts
+++ src/reducer/schematics.ts
@@ -13,57 +13,61 @@ const init: State = {
   active: [2],
 };
 
+const advance = (id: number[], i: number, k: number) => [...id.slice(0, i), id[i] + k, ...id.slice(i + 1)];
+
+const patch = (entry: Element, path: number[], f: (_: Element) => Element): Element => {
+  if (path.length === 1 && path[0] === 0) {
+    return f(entry);
+  } else if (path.length > 1 && path[0] === 0) {
+    if (entry.kind !== Kind.shunt) {
+      throw new Error(`expected '${Kind.shunt}', got '${entry.kind}'`);
+    }
+
+    const next = patch(entry.value, path.slice(1), f);
+
+    if (next.kind !== entry.value.kind) {
+      throw new Error(`expected '${entry.value.kind}', got '${next.kind}'`);
+    }
+
+    return shunt(entry.next, next);
+
+  } else {
+    if (entry.kind === Kind.connector) {
+      throw new Error(`unexpected '${Kind.connector}'`);
+    }
+
+    return make(entry.kind, patch(entry.next, advance(path, 0, -1), f), entry.value);
+  }
+};
+
 export const reducer: Reducer<State> = (state = init, action: Action): State => {
   switch (action.type) {
     case Type.activate:
       return {...state, active: action.id};
 
     case Type.insert:
-      if (state.active.length === 1 && state.active[0] === 0) {
-        return ({
-          entry: make(action.kind, state.entry),
-          active: [1],
-        });
-      } else if (state.active.length > 1 && state.active[0] === 0) {
-        if (state.entry.kind !== Kind.shunt) {
-          throw new Error(`expected '${Kind.shunt}', got '${state.entry.kind}'`);
-        }
+      return {
+        entry: patch(state.entry, state.active, (entry) => make(action.kind, entry)),
+        active: advance(state.active, state.active.length - 1, 1),
+      };
 
-        const nested = reducer(
-          {
-            entry: state.entry.value,
-            active: state.active.slice(1),
-          },
-          action,
-        );
+    case Type.remove:
+      const lead = action.id.slice(0, -1);
 
-        if (nested.entry.kind !== Kind.series) {
-          throw new Error(`expected '${Kind.series}', got '${nested.entry.kind}'`);
-        }
+      return {
+        entry: patch(state.entry, action.id, (entry) => {
+          if (entry.kind === Kind.connector) {
+            throw new Error(`unexpected '${Kind.connector}'`);
+          }
 
-        return ({
-          entry: shunt(state.entry.next, nested.entry),
-          active: [0, ...nested.active],
-        });
-
-      } else {
-        if (state.entry.kind === Kind.connector) {
-          throw new Error(`unexpected '${Kind.connector}'`);
-        }
-
-        const nested = reducer(
-          {
-            entry: state.entry.next,
-            active: [state.active[0] - 1, ...state.active.slice(1)],
-          },
-          action,
-        );
-
-        return ({
-          entry: make(state.entry.kind, nested.entry, state.entry.value),
-          active: [nested.active[0] + 1, ...nested.active.slice(1)],
-        });
-      }
+          return entry.next;
+        }),
+        active: !(
+          lead.length < state.active.length
+          && lead.every((x, i) => x === state.active[i])
+          && action.id[lead.length] < state.active[lead.length]
+        ) ? state.active : advance(state.active, lead.length, -1),
+      };
 
     default:
       return state;
