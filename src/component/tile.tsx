b6ede6800d8ad443841e797407b77306ebf62378
Bruno Dutra <brunocodutra@gmail.com>
2018-01-27 16:48:43 +0100

    extract event adaptors to lib/event

--- src/component/tile.tsx
+++ src/component/tile.tsx
@@ -1,30 +1,13 @@
 import * as classes from 'classnames';
 import * as React from 'react';
 
-const lift = (f?: () => void) => f && (
-  (_: React.SyntheticEvent<HTMLElement>) => f()
-);
-
-const isolate = (f?: (_: React.SyntheticEvent<HTMLElement>) => void) => f && (
-  (e: React.SyntheticEvent<HTMLElement>) => {
-    e.stopPropagation();
-    f(e);
-  }
-);
-
-const adapt = (f?: (_: React.SyntheticEvent<HTMLElement>) => void) => f && (
-  (e: React.KeyboardEvent<HTMLElement>) => {
-    if (e.key === ' ' || e.key === 'Enter') {
-      f(e);
-    }
-  }
-);
+import {adapt, isolate} from 'lib/event';
 
 const Remove = ({event}: {event?: () => void}) => !event ? null : (
   <span
     className='control remove'
-    onMouseDown={isolate(lift(event))}
-    onKeyDown={adapt(isolate(lift(event)))}
+    onMouseDown={isolate(event)}
+    onKeyDown={event && adapt([' ', 'Enter'], isolate(event))}
     tabIndex={0}
   />
 );
@@ -47,8 +30,8 @@ export default ({active = false, activate, remove, className = '', children}: Pr
   return (
     <span
       className={classes('tile', className, {active, interactive})}
-      onMouseDown={isolate(lift(activate))}
-      onKeyDown={adapt(isolate(lift(activate)))}
+      onMouseDown={activate && isolate(activate)}
+      onKeyDown={activate && adapt([' ', 'Enter'], isolate(activate))}
       tabIndex={interactive ? 0 : -1}
     >
       <Remove event={remove}/>
