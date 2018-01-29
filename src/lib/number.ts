355f2f3f99fd6350c9ffbd127c1b661c8c474760
Bruno Dutra <brunocodutra@gmail.com>
2018-01-29 23:13:57 +0100

    add a slightly relaxed number parser

--- src/lib/number.ts
+++ src/lib/number.ts
@@ -3,3 +3,23 @@ export const pretty = (x: number, d = 3): string => {
   const k = ((x < 0) ? -1 : 1) * 10 ** f;
   return (Math.floor(x * k) / k).toFixed(f);
 };
+
+export const parse = (s: string): number => {
+  s = s.trimLeft();
+
+  return (
+      (/^\-\.?(e|E)/.test(s))
+    ? -+`1${s.slice(1)}`
+    : (s.startsWith('-'))
+    ? -+`0${s.slice(1)}`
+    : (/^\+\.?(e|E)/.test(s))
+    ? +`1${s.slice(1)}`
+    : (s.startsWith('+'))
+    ? +`0${s.slice(1)}`
+    : (/^\.?(e|E)/.test(s))
+    ? +`1${s}`
+    : (s.startsWith('.'))
+    ? +`0${s}`
+    : +s
+  );
+};
