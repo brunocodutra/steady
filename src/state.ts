48aaf80ade5067fe88a037d92ca991b2d72891db
Bruno Dutra <brunocodutra@gmail.com>
2018-02-15 22:59:58 +0100

    make State serializable

--- src/state.ts
+++ src/state.ts
@@ -1,3 +1,5 @@
+import {compressToEncodedURIComponent as encode, decompressFromEncodedURIComponent as decode} from 'lz-string';
+
 import {Element, series} from 'lib/element';
 
 export type State = {
@@ -8,4 +10,18 @@ export type State = {
 export const init: State = {
   entry: series(),
   active: [2],
+};
+
+// best effort
+const isState = (s: any): s is State => typeof s === 'object' && 'entry' in s && 'active' in s;
+
+export const serialize = (state: State): string => encode(JSON.stringify(state));
+
+export const unserialize = (encoded: string): State | undefined => {
+  try {
+    const obj = JSON.parse(decode(encoded));
+    return isState(obj) ? obj : undefined;
+  } catch (_) {
+    return undefined;
+  }
 };
