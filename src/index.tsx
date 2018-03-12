2a8802184c076bb27d44557bfbe5f5e62da5fbd0
Bruno Dutra <brunocodutra@gmail.com>
2018-03-12 22:21:50 +0100

    fix hot reloading

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-import {AppContainer} from 'react-hot-loader';
 import {Provider} from 'react-redux';
 
 import Steady from 'component/steady';
@@ -9,26 +7,20 @@ import Steady from 'component/steady';
 import {unwrap} from 'lib/util';
 import store from 'store';
 
-const render = (component: JSX.Element, placeholder: HTMLElement) => ReactDOM.render(
-  <AppContainer>
-    <Provider store={store}>
-      {component}
-    </Provider>
-  </AppContainer>,
-  placeholder,
+ReactDOM.render(
+  <Provider store={store}>
+    <Steady/>
+  </Provider>,
+  unwrap(document.getElementById('steady'), '#steady not found'),
 );
-
-const steady = unwrap(document.getElementById('steady'), '#steady not found');
-
-render(<Steady/>, steady);
 
 /* istanbul ignore next */
 if (module.hot) {
-  module.hot.accept('./reducer.ts', () => {
+  module.hot.accept('./store.ts', () => {
     store.replaceReducer(require('reducer').default);
   });
 
-  module.hot.accept('./component/steady.tsx', () => {
-    render(React.createElement(require('component/steady').default), steady);
+  module.hot.accept('./reducer.ts', () => {
+    store.replaceReducer(require('reducer').default);
   });
 }
