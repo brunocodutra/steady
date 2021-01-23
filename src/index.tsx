916c17d72558acbf8427354044eb4203e6cd696b
Bruno Dutra <brunocodutra@gmail.com>
2021-01-23 16:46:40 +0100

    remove /* istanbul ignore next */ from production code

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,10 @@ export default Promise.all([import('react'), import('react-dom')]).then(async ([
   ReactDOM.render(<Steady />, document.getElementById('steady'));
 });
 
-/* istanbul ignore next */
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    try {
-      console.log('SW registered: ', await navigator.serviceWorker.register('/steady/sw.js'));
-    } catch (error) {
-      console.error('SW registration failed: ', error);
-    }
-  });
-}
+window.addEventListener('load', async () => {
+  try {
+    console.log('SW registered: ', await navigator.serviceWorker.register('/steady/sw.js'));
+  } catch (error) {
+    console.error('SW registration failed: ', error);
+  }
+});
