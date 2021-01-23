5321302c4a8eaffb0ae2d28268e70d28932c1719
Bruno Dutra <brunocodutra@gmail.com>
2021-01-23 16:06:47 +0100

    test the unhappy case when conainers are missing

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,7 +1,25 @@
 describe('App', () => {
-  it('should mount', () => {
+  it('should mount if #navbar, #toggler, and #steady exist', () => {
     document.body.innerHTML = '<div id="navbar"></div><div id="toggler"></div><div id="steady"></div>';
 
-    require('index');
+    expect(require('index').default).resolves;
+  });
+
+  it('should fail to mount if #navbar is missing', () => {
+    document.body.innerHTML = '<div></div><div id="toggler"></div><div id="steady"></div>';
+
+    expect(require('index').default).rejects;
+  });
+
+  it('should fail to mount if #toggler is missing', () => {
+    document.body.innerHTML = '<div id="navbar"></div><div></div><div id="steady"></div>';
+
+    expect(require('index').default).rejects;
+  });
+
+  it('should fail to mount if #steady is missing', () => {
+    document.body.innerHTML = '<div id="navbar"></div><div id="toggler"></div><div></div>';
+
+    expect(require('index').default).rejects;
   });
 });
