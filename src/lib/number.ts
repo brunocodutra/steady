bb094d194bcf48b0e0765e3aa5eccb7c8d56a6ce
Bruno Dutra <brunocodutra@gmail.com>
2021-01-24 18:21:51 +0100

    refactor logic that formats quantities

--- src/lib/number.ts
+++ src/lib/number.ts
@@ -1,7 +1,16 @@
 export const pretty = (x: number, d = 3): string => {
-  const f = d - 1 - Math.min(Math.max(Math.floor(Math.log10(Math.abs(x))), 0), d - 1);
-  const k = ((x < 0) ? -1 : 1) * 10 ** f;
-  return (Math.floor(x * k) / k).toFixed(f);
+  switch (x) {
+    case Infinity:
+      return '∞';
+
+    case -Infinity:
+      return '-∞';
+
+    default:
+      const f = d - 1 - Math.min(Math.max(Math.floor(Math.log10(Math.abs(x))), 0), d - 1);
+      const k = ((x < 0) ? -1 : 1) * 10 ** f;
+      return (Math.floor(x * k) / k).toFixed(f);
+  }
 };
 
 export const parse = (s: string): number => {
